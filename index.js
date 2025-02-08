1757c4b863363e497a2033970a8963c89f418b66
vsots <vsotsdev@gmail.com>
2025-02-08 15:25:30 -0800

    refactor for prod

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
+import dotenv from 'dotenv';
 import * as http from 'http';
 import * as nodemailer from 'nodemailer';
 import contactFormValSan from './modules/contactFormValSan.js';
 
-const host = '127.0.0.1';
-const port = 8080;
+dotenv.config();
 
 const emailBody =  (json) => (
 `Name: ${json.name}
-email: ${json.email}
+Email: ${json.email}
+Subject: ${json.subject}
 
 
 Message:
 ${json.message}`);
 
+const port = 8080;
 const server = http.createServer(handler);
-server.listen(port, host, () => {
-    console.log(`Server Listening At http://${host}:${port}/`);
+server.listen(port, () => {
+    console.log("Server Listening on Port: ", port);
 })
 
 function handler(req, res) {
-    const headers = {
-        'Access-Control-Allow-Origin': 'http://localhost:3000',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
-    };
+    // uncomment for localhost
+    // const headers = {
+    //     'Access-Control-Allow-Origin': 'http://localhost:3000',
+    //     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    //     'Access-Control-Allow-Headers': 'Content-Type'
+    // };
 
-    if (req.method === 'OPTIONS') res.writeHead(200, headers).end();
-    else if (req.method === 'POST' && req.url === '/contactsubmission') {
+    // if (req.method === 'OPTIONS') res.writeHead(200, headers).end();
+
+    if (req.method === 'POST' && req.url === '/contactsubmission') {
         let data = '';
         req.on('data', chunk => data += chunk);
         req.on('end', () => {
@@ -36,20 +40,20 @@ function handler(req, res) {
                 res.end('Form Validation Failed');
             } else {
                 const transporter = nodemailer.createTransport({
-                    host: 'smtp.host.server',
+                    host: process.env.SMTP_HOST,
                     port: 465,
                     secure: true,
                     auth: {
-                        user: "mail@example.com",
-                        pass: "___"
+                        user: process.env.EMAIL_FROM,
+                        pass: process.env.SMTP_PASS
                     }
                   });
                 
                 const mailText = emailBody(valid);
 
                 const mailOptions = {
-                    to: 'anothermail@example.com',
-                    subject: valid.subject,
+                    to: process.env.EMAIL_TO,
+                    subject: "NEW MAIL FROM PERSONAL WEBSITE",
                     text: mailText
                 }
 
@@ -61,12 +65,14 @@ function handler(req, res) {
                     }
                 })
 
-                res.writeHead(204, headers);
+                // res.writeHead(204, headers);
+                res.writeHead(204);
                 res.end();
             }
         })
     } else {
-        res.writeHead(400, { ...headers, 'Content-Type': 'text/plain' });
+        //res.writeHead(400, { ...headers, 'Content-Type': 'text/plain' });
+        res.writeHead(400, {'Content-Type': 'text/plain' });
         res.end('Unknown Request');
     }
 }
